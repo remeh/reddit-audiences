e9bfb0ae28967bb502cd0c563e6461f2fac8b4eb
remeh <remeh@remeh.fr>
2016-02-03 13:05:35 +0100

    server: avoid divide by zero...

--- app/math.go
+++ app/math.go
@@ -7,5 +7,10 @@ func Average(audiences []Audience) int64 {
 	for _, a := range audiences {
 		rv += int64(a.Audience)
 	}
+
+	if len(audiences) == 0 {
+		return 0
+	}
+
 	return rv / int64(len(audiences))
 }
