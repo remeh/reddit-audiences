27f8aaae4d7ffca811c9505c1f5142aa21e5386e
remeh <remeh@remeh.fr>
2016-02-07 17:26:22 +0100

    web+api: add article monitoring.

---
+++ api/object/article.go
@@ -0,0 +1,51 @@
+package object
+
+import (
+	"github.com/remeh/reddit-audiences/app"
+)
+
+type Article struct {
+	ArticleId    string `json:"article_id"`
+	ArticleTitle string `json:"article_title"`
+	ArticleLink  string `json:"article_link"`
+	Author       string `json:"author"`
+	Promoted     bool   `json:"promoted"`
+	Sticky       bool   `json:"sticky"`
+	MinRank      int    `json:"min_rank"`
+	MaxRank      int    `json:"max_rank"`
+	//Ranking      []Ranking `json:"ranking"`
+}
+
+func ArticlesFromApp(articles []app.Article, rankings Rankings) []Article {
+	rv := make([]Article, len(articles))
+	for i, a := range articles {
+		rv[i] = ArticleFromApp(a, rankings[a.ArticleId])
+	}
+	return rv
+}
+
+func ArticleFromApp(article app.Article, ranking []Ranking) Article {
+	var min, max int
+	min = 10E6
+
+	for _, r := range ranking {
+		if r.Rank > max {
+			max = r.Rank
+		}
+		if r.Rank < min {
+			min = r.Rank
+		}
+	}
+
+	return Article{
+		ArticleId:    article.ArticleId,
+		ArticleTitle: article.ArticleTitle,
+		ArticleLink:  article.ArticleLink,
+		Author:       article.Author,
+		Promoted:     article.Promoted,
+		Sticky:       article.Sticky,
+		MinRank:      min,
+		MaxRank:      max,
+		//Ranking:      ranking,
+	}
+}
