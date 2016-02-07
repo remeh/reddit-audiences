d550f55fb0e48f39d466ba209cda408feea3236c
remeh <remeh@remeh.fr>
2016-02-07 18:43:42 +0100

    app: add some UA.

--- app/http_client.go
+++ app/http_client.go
@@ -8,6 +8,9 @@ import (
 var useragents []string = []string{
 	"Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0",
 	"Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0",
+	"Mozilla/5.0 (Linux; Android 5.1; XT1039 Build/LPB23.13-17.6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.95 Mobile Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36",
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36",
 }
 
 func NewRequest(url string) (*http.Request, error) {
