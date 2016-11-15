c0ea42e75c6ca4ad43b97ad5c2b81a032ddba546
remeh <me@remy.io>
2016-11-15 20:38:53 +0100

    today: limit to 2016-11-03

--- api/today_handler.go
+++ api/today_handler.go
@@ -109,8 +109,8 @@ func (c TodayHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (c TodayHandler) getData(subreddit string, hours int) ([]db.Audience, map[string][]db.Ranking, []db.Article, error) {
 	var start, end time.Time
 
-	end = time.Now()
-	start = time.Now().Add(-time.Hour * time.Duration(hours))
+	end = time.Date(2016, time.November, 03, 10, 00, 00, 00, time.UTC())
+	start = end.Add(-time.Hour * time.Duration(hours))
 
 	audiences, err := c.App.DB().FindAudiencesInterval(subreddit, start, end)
 	if err != nil {
